ec8574841d1509230d499a6df7b521370031cc4c
venkatakarthik91 <venkatakarthik91@outlook.com>
2016-01-08 01:13:55 +0530

    dgd

--- Indian Caloricator/www/scripts/controllers.js
+++ Indian Caloricator/www/scripts/controllers.js
@@ -1,7 +1,16 @@
-﻿/// <reference path=".\angular.js" />
-var caloricator = angular.module("caloricator", []);
-
-caloricator.controller("CaloricatorController", function ($scope, $http) {
+﻿/// <reference path=".\angular.js"/>
+/// <reference path=".\angular-route.js" />
+var caloricator = angular.module("caloricator", ["ngRoute", "caloricatorControllers"]);
+caloricator.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+          templateUrl: '/partials/home.html'//,
+          //controller: 'CaloricatorController'
+      }).otherwise({
+          templateUrl: '/partials/home.html'
+      });
+});
+var caloricatorControllers = angular.module("caloricatorControllers", []);
+caloricatorControllers.controller("CaloricatorController", function ($scope, $http) {
     var promise = $http.get("http://caloricator.azurewebsites.net/api/calorie");
     promise.then(function successCallback(response) {
         $scope.benefitsOfCounting = response.data;
